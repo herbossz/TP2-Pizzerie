bf39a798f9381f844e4d3eed728d6c4778132017
Heri Ezekiya <ezekia28@gmail.com>
2021-03-09 23:05:33 -0500

    ajjout des codes fonctions et classes

--- lesClasses.js
+++ lesClasses.js
@@ -1,3 +1,79 @@
 
 // fichier classes de la pizzeria
 // fait par Heri Ezekiya && Nunsseu Elise Man
+
+
+// classe aliment
+class aliment{
+    constructor(nom,poids){
+        this.nom = nom;
+        this.poids= poids;
+    }
+}
+
+//classe croute
+class croute extends aliment{
+    constructor (prix,taille){
+        this.prix = prix;
+        this.taille = taille;
+
+    }
+}
+
+// classe fromage
+class fromage extends croute{
+    constructor (prix){
+        this.prix = prix;
+    }
+}
+
+// classe Garniture
+class Garniture extends fromage{
+    constructor (prix){
+    this.prix = prix;
+
+    }
+}
+
+// classe epices
+class epices extends aliment{
+    constructor (Epice){
+
+        this.eEpice = Epice;
+    }
+}
+
+//classes pizza
+class pizza {
+    constructor (numeroPizza,taille,croute,fromage,Garniture,prixComplet,tempsCuisson){
+        this.numeroPizza = numeroPizza;
+        this.taille = taille;
+        this.croute = croute;
+        this.fromage = fromage;
+        this.Garniture = Garniture;
+        this.prixComplet = prixComplet;
+        this.tempsCuisson = tempsCuisson;
+    }
+}
+
+// classe commande
+class commande {
+    constructor (numeroComande,date,heure,pizza,montantTotal){
+        this.numeroComande = numeroComande;
+        this.dateCommande = date;
+        this.heure = heure;
+        this.pizza = pizza;
+        this.montantTotal= montantTotal;
+    }
+}
+
+// classe client
+class client {
+    constructor(nom, prenom,telephone,courriel,commandeClient){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone=telephone;
+        this.courriel =courriel;
+        this.commandeClient = commandeClient;
+    }
+}
--- lesFonctions.js
+++ lesFonctions.js
@@ -1,2 +1,51 @@
 // fichier fonctions de la pizzeria
 // fait par Heri Ezekiya && Nunsseu Elise Man
+
+
+// 1.  verifierNumeroDeTelephone(numero)
+console.log("================================================");
+console.log("================================================");
+ 
+function verifierNumeroDeTelephone (numero)
+{
+
+  if(numero.match(/^\d+$/) && (numero.length ==9)){
+    console.log("Résultat optenu : Vrai")
+  
+  }else{
+    console.log("Résultat optenu : Faut")
+  }
+  
+}
+
+// 2.  verifierEntier(entier)
+
+function verifierEntier (entier)
+{
+
+  if(entier.match(/^-{0,1}\d+$/)){
+    console.log("Résultat optenu : Vrai")
+  
+  }else{
+    console.log("Résultat optenu : Faut")
+  }
+}
+
+//3. verifierCodePostal(codePostal)
+console.log("3. verifierCodePostal(codePostal)")
+
+function verifierCodePostal(codePostal)
+{
+
+ regexp = /^[a-zA-Z]{1}[0-9]{1}[a-zA-Z]{1}[0-9]{1}[a-zA-Z]{1}[0-9]{1}?$/
+  
+        if (regexp.test(codePostal))
+          {
+            return "Résultat obtenu: vrai";
+          }
+        else
+          {
+            return "Résultat obtenu: Faut";
+          }
+}
+
