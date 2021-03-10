6235ad301e72f2c21878c503a204babe16ea2d8e
Heri Ezekiya <ezekia28@gmail.com>
2021-03-09 22:37:00 -0500

    creeation des fichier fonctions.js et classes.js et ajjout des commentaires

---
+++ lesClasses.js
@@ -0,0 +1,3 @@
+
+// fichier classes de la pizzeria
+// fait par Heri Ezekiya && Nunsseu Elise Man
---
+++ lesFonctions.js
@@ -0,0 +1,2 @@
+// fichier fonctions de la pizzeria
+// fait par Heri Ezekiya && Nunsseu Elise Man
